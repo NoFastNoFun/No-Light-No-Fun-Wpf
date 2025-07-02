c3b39c02a6b1202f12a18a6fe2d4c9c43a055770
benjamin <bandasavanh.b@akira-sarl.fr>
2025-07-02 14:35:42 +0200

    update streamamanger & his view

--- ViewModels/StreamManagerViewModel.cs
+++ ViewModels/StreamManagerViewModel.cs
@@ -1,12 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Models;
+using System.Windows.Input;
 
 namespace No_Fast_No_Fun_Wpf.ViewModels
 {
-    class StreamManagerViewModel : BaseViewModel
-    {
+    public class StreamManagerViewModel : BaseViewModel {
+        public ObservableCollection<StreamItem> Streams {
+            get;
+        }
+
+        StreamItem? _selectedStream;
+        public StreamItem? SelectedStream {
+            get => _selectedStream;
+            set {
+                if (SetProperty(ref _selectedStream, value)) {
+                    (RemoveStreamCmd as RelayCommand)?.RaiseCanExecuteChanged();
+                    (ToggleStreamCmd as RelayCommand)?.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        public ICommand AddStreamCmd {
+            get;
+        }
+        public ICommand RemoveStreamCmd {
+            get;
+        }
+        public ICommand ToggleStreamCmd {
+            get;
+        }
+
+        public StreamManagerViewModel() {
+            Streams = new ObservableCollection<StreamItem>();
+
+            AddStreamCmd = new RelayCommand(_ => AddStream());
+            RemoveStreamCmd = new RelayCommand(_ => RemoveStream(), _ => SelectedStream != null);
+            ToggleStreamCmd = new RelayCommand(_ => ToggleStream(), _ => SelectedStream != null);
+        }
+
+        void AddStream() {
+            var stream = new StreamItem {
+                Name = $"Flux {Streams.Count + 1}",
+                Universe = 0,
+                IsActive = false
+            };
+            Streams.Add(stream);
+            SelectedStream = stream;
+        }
+
+        void RemoveStream() {
+            if (SelectedStream != null) {
+                Streams.Remove(SelectedStream);
+                SelectedStream = null;
+            }
+        }
+
+        void ToggleStream() {
+            if (SelectedStream != null)
+                SelectedStream.IsActive = !SelectedStream.IsActive;
+            // Notify UI if needed (StreamItem pourrait implémenter INotifyPropertyChanged si modifié dynamiquement)
+        }
     }
 }
---
+++ Views/Controls/StreamManagerControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace No_Fast_No_Fun_Wpf.Views.Controls
+{
+    /// <summary>
+    /// Logique d'interaction pour StreamsView.xaml
+    /// </summary>
+    public partial class StreamManagerControl : UserControl
+    {
+        public StreamManagerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
